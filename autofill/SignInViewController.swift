43d4af5f0eba1288b84d704b8b11a13c2b6bc81d
Alex Kisel <alexander.kisel@brander.ua>
2018-06-07 18:31:38 +0300

    Sample project

---
+++ autofill/SignInViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SignInViewController.swift
+//  autofill
+//
+//  Created by Alex Kisel on 6/7/18.
+//  Copyright © 2018 Alex Kisel. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+}
---
+++ autofill/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  autofill
+//
+//  Created by Alex Kisel on 6/7/18.
+//  Copyright © 2018 Alex Kisel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+}
+
